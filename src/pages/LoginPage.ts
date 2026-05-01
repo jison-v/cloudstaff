23c71dab28960395acedee728d9bf1dd2daa6915
jay.villacite <jaysonvillacite@gmail.com>
2026-05-01 10:51:40 +0800

    Refactor LoginPage to include login functionality

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -2,18 +2,22 @@ import { expect, type Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class ExampleHomePage extends BasePage {
-  readonly heroTitle = this.page.locator('h1');
-  readonly firstParagraph = this.page.locator('p').first();
+  readonly usernameInput = this.page.locator('input[name="username"]');
+  readonly passwordInput = this.page.locator('input[name="password"]');
+  readonly signInButton = this.page.locator('button:has-text("Sign In")');
+
 
   async goto() {
     await super.goto('https://app.actimo.com/admin');
   }
 
-  async expectHeroTitle() {
-    await expect(this.heroTitle).toHaveText('Example Domain');
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.signInButton.click();
   }
 
-  async expectFirstParagraph() {
-    await expect(this.firstParagraph).toContainText('This domain is for use in documentation examples');
+  async expectSignInButtonVisible() {
+    await expect(this.signInButton).toBeVisible();
   }
 }
